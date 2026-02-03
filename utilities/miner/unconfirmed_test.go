39e622dd39741fe7143c9bd9cc142a02bb924281
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-02-03 05:39:49 +0200

    Create unconfirmed_test.go

---
+++ utilities/miner/unconfirmed_test.go
@@ -0,0 +1,62 @@
+package miner
+
+import (
+	"testing"
+
+	"github.com/skaindev/skaina/chain/core/types"
+	"github.com/skaindev/skaina/utilities/common"
+)
+
+type noopHeaderRetriever struct{}
+
+func (r *noopHeaderRetriever) GetHeaderByNumber(number uint64) *types.Header {
+	return nil
+}
+
+func TestUnconfirmedInsertBounds(t *testing.T) {
+	limit := uint(10)
+
+	pool := newUnconfirmedBlocks(new(noopHeaderRetriever), limit)
+	for depth := uint64(0); depth < 2*uint64(limit); depth++ {
+
+		for i := 0; i < int(depth); i++ {
+			pool.Insert(depth, common.Hash([32]byte{byte(depth), byte(i)}))
+		}
+
+		pool.blocks.Do(func(block interface{}) {
+			if block := block.(*unconfirmedBlock); block.index+uint64(limit) <= depth {
+				t.Errorf("depth %d: block %x not dropped", depth, block.hash)
+			}
+		})
+	}
+}
+
+func TestUnconfirmedShifts(t *testing.T) {
+
+	limit, start := uint(10), uint64(25)
+
+	pool := newUnconfirmedBlocks(new(noopHeaderRetriever), limit)
+	for depth := start; depth < start+uint64(limit); depth++ {
+		pool.Insert(depth, common.Hash([32]byte{byte(depth)}))
+	}
+
+	pool.Shift(start + uint64(limit) - 1)
+	if n := pool.blocks.Len(); n != int(limit) {
+		t.Errorf("unconfirmed count mismatch: have %d, want %d", n, limit)
+	}
+
+	pool.Shift(start + uint64(limit) - 1 + uint64(limit/2))
+	if n := pool.blocks.Len(); n != int(limit)/2 {
+		t.Errorf("unconfirmed count mismatch: have %d, want %d", n, limit/2)
+	}
+
+	pool.Shift(start + 2*uint64(limit))
+	if n := pool.blocks.Len(); n != 0 {
+		t.Errorf("unconfirmed count mismatch: have %d, want %d", n, 0)
+	}
+
+	pool.Shift(start + 3*uint64(limit))
+	if n := pool.blocks.Len(); n != 0 {
+		t.Errorf("unconfirmed count mismatch: have %d, want %d", n, 0)
+	}
+}
