4eb68121a472dddaf3a20deef43adcc1f89adead
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-02-09 22:02:28 +0200

    Create tx_list_test.go

---
+++ chain/core/tx_list_test.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/skaindev/skaina/chain/core/types"
+	"github.com/skaindev/skaina/utilities/crypto"
+)
+
+func TestStrictTxListAdd(t *testing.T) {
+	key, _ := crypto.GenerateKey()
+
+	txs := make(types.Transactions, 1024)
+	for i := 0; i < len(txs); i++ {
+		txs[i] = transaction(uint64(i), 0, key)
+	}
+	list := newTxList(true)
+	for _, v := range rand.Perm(len(txs)) {
+		list.Add(txs[v], DefaultTxPoolConfig.PriceBump)
+	}
+	if len(list.txs.items) != len(txs) {
+		t.Errorf("transaction count mismatch: have %d, want %d", len(list.txs.items), len(txs))
+	}
+	for i, tx := range txs {
+		if list.txs.items[tx.Nonce()] != tx {
+			t.Errorf("item %d: transaction mismatch: have %v, want %v", i, list.txs.items[tx.Nonce()], tx)
+		}
+	}
+}
