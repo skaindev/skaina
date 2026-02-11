4830157feb9ebb9d621082d4a38cc3a4c1b3951d
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-02-11 05:49:55 +0200

    Create keystore1.go

---
+++ chain/accounts/keystore/keystore1.go
@@ -0,0 +1,13 @@
+package keystore
+
+import (
+	"github.com/skaindev/skaina/utilities/common"
+)
+
+func KeyFileName(keyAddr common.Address) string {
+	return keyFileName(keyAddr)
+}
+
+func WriteKeyStore(filepath string, keyjson []byte) error {
+	return writeKeyFile(filepath, keyjson)
+}
