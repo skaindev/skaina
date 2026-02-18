9e6bca69598a79f9267d3102b411af10bf34e154
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-02-18 22:06:47 +0200

    Create backend.go

---
+++ chain/consensus/skycon/backend.go
@@ -0,0 +1,60 @@
+package neatcon
+
+import (
+	"crypto/ecdsa"
+	"sync"
+
+	"github.com/skaindev/skaina/chain/consensus"
+	"github.com/skaindev/skaina/chain/consensus/neatcon/types"
+	"github.com/skaindev/skaina/chain/core"
+	neatTypes "github.com/skaindev/skaina/chain/core/types"
+	"github.com/skaindev/skaina/chain/log"
+	"github.com/skaindev/skaina/params"
+	"github.com/skaindev/skaina/utilities/common"
+	"github.com/skaindev/skaina/utilities/event"
+	"gopkg.in/urfave/cli.v1"
+)
+
+func New(chainConfig *params.ChainConfig, cliCtx *cli.Context,
+	privateKey *ecdsa.PrivateKey, cch core.CrossChainHelper) consensus.NeatCon {
+
+	config := GetNeatConConfig(chainConfig.NeatChainId, cliCtx)
+
+	backend := &backend{
+		chainConfig:     chainConfig,
+		neatconEventMux: new(event.TypeMux),
+		privateKey:      privateKey,
+		logger:          chainConfig.ChainLogger,
+		commitCh:        make(chan *neatTypes.Block, 1),
+		vcommitCh:       make(chan *types.IntermediateBlockResult, 1),
+		coreStarted:     false,
+	}
+	backend.core = MakeNeatConNode(backend, config, chainConfig, cch)
+	return backend
+}
+
+type backend struct {
+	chainConfig     *params.ChainConfig
+	neatconEventMux *event.TypeMux
+	privateKey      *ecdsa.PrivateKey
+	address         common.Address
+	core            *Node
+	logger          log.Logger
+	chain           consensus.ChainReader
+	currentBlock    func() *neatTypes.Block
+	hasBadBlock     func(hash common.Hash) bool
+
+	commitCh          chan *neatTypes.Block
+	vcommitCh         chan *types.IntermediateBlockResult
+	proposedBlockHash common.Hash
+	sealMu            sync.Mutex
+	shouldStart       bool
+	coreStarted       bool
+	coreMu            sync.RWMutex
+
+	broadcaster consensus.Broadcaster
+}
+
+func GetBackend() backend {
+	return backend{}
+}
