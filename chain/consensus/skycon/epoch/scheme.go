b0cd645fb11f4686fe010f59da5c3af246c4fa6a
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-02-20 19:13:02 +0200

    Create scheme.go

---
+++ chain/consensus/skycon/epoch/scheme.go
@@ -0,0 +1,70 @@
+package epoch
+
+import (
+	"fmt"
+	"math/big"
+	"sync"
+
+	dbm "github.com/skaindev/db-go"
+	ncTypes "github.com/skaindev/skaina/chain/consensus/neatcon/types"
+	"github.com/skaindev/skaina/chain/log"
+	"github.com/skaindev/wire-go"
+)
+
+const rewardSchemeKey = "REWARDSCHEME"
+
+type RewardScheme struct {
+	mtx sync.Mutex
+	db  dbm.DB
+
+	TotalReward        *big.Int
+	RewardFirstYear    *big.Int
+	EpochNumberPerYear uint64
+	TotalMintingYears  uint64
+}
+
+func LoadRewardScheme(db dbm.DB) *RewardScheme {
+	buf := db.Get([]byte(rewardSchemeKey))
+	if len(buf) == 0 {
+		return nil
+	} else {
+		rs := &RewardScheme{}
+		err := wire.ReadBinaryBytes(buf, rs)
+		if err != nil {
+			log.Errorf("LoadRewardScheme Failed, error: %v", err)
+			return nil
+		}
+		return rs
+	}
+}
+
+func MakeRewardScheme(db dbm.DB, rsDoc *ncTypes.RewardSchemeDoc) *RewardScheme {
+
+	rs := &RewardScheme{
+		db:                 db,
+		TotalReward:        rsDoc.TotalReward,
+		RewardFirstYear:    rsDoc.RewardFirstYear,
+		EpochNumberPerYear: rsDoc.EpochNumberPerYear,
+		TotalMintingYears:  rsDoc.TotalMintingYears,
+	}
+
+	return rs
+}
+
+func (rs *RewardScheme) Save() {
+	rs.mtx.Lock()
+	defer rs.mtx.Unlock()
+	rs.db.SetSync([]byte(rewardSchemeKey), wire.BinaryBytes(*rs))
+}
+
+func (rs *RewardScheme) String() string {
+
+	return fmt.Sprintf("RewardScheme : {"+
+		"totalReward : %v,\n"+
+		"rewardFirstYear : %v,\n"+
+		"epochNumberPerYear : %v,\n"+
+		"}",
+		rs.TotalReward,
+		rs.RewardFirstYear,
+		rs.EpochNumberPerYear)
+}
