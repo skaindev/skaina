009f974830769be10747ca089784144e9b969e3c
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-02-23 17:37:13 +0200

    Create signable.go

---
+++ chain/consensus/skycon/types/signable.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	"bytes"
+	"io"
+
+	. "github.com/skaindev/common-go"
+	"github.com/skaindev/merkle-go"
+)
+
+type Signable interface {
+	WriteSignBytes(chainID string, w io.Writer, n *int, err *error)
+}
+
+func SignBytes(chainID string, o Signable) []byte {
+	buf, n, err := new(bytes.Buffer), new(int), new(error)
+	o.WriteSignBytes(chainID, buf, n, err)
+	if *err != nil {
+		PanicCrisis(err)
+	}
+	return buf.Bytes()
+}
+
+func HashSignBytes(chainID string, o Signable) []byte {
+	return merkle.SimpleHashFromBinary(SignBytes(chainID, o))
+}
