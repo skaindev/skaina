c407a90c22331b13d8b485b15d49ffb6ddd63f97
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-03-03 20:11:44 +0200

    Create state_transition.go

---
+++ chain/core/state_transition.go
@@ -0,0 +1,296 @@
+package core
+
+import (
+	"errors"
+	"math"
+	"math/big"
+
+	"github.com/skaindev/skaina/chain/core/vm"
+	"github.com/skaindev/skaina/params"
+	"github.com/skaindev/skaina/utilities/common"
+)
+
+var (
+	errInsufficientBalanceForGas = errors.New("insufficient balance to pay for gas")
+)
+
+type StateTransition struct {
+	gp         *GasPool
+	msg        Message
+	gas        uint64
+	gasPrice   *big.Int
+	initialGas uint64
+	value      *big.Int
+	data       []byte
+	state      vm.StateDB
+	evm        *vm.EVM
+}
+
+type Message interface {
+	From() common.Address
+
+	To() *common.Address
+
+	GasPrice() *big.Int
+	Gas() uint64
+	Value() *big.Int
+
+	Nonce() uint64
+	CheckNonce() bool
+	Data() []byte
+}
+
+type ExecutionResult struct {
+	UsedGas    uint64
+	Err        error
+	ReturnData []byte
+}
+
+func (result *ExecutionResult) Unwrap() error {
+	return result.Err
+}
+
+func (result *ExecutionResult) Failed() bool { return result.Err != nil }
+
+func (result *ExecutionResult) Return() []byte {
+	if result.Err != nil {
+		return nil
+	}
+	return common.CopyBytes(result.ReturnData)
+}
+
+func (result *ExecutionResult) Revert() []byte {
+	if result.Err != vm.ErrExecutionReverted {
+		return nil
+	}
+	return common.CopyBytes(result.ReturnData)
+}
+
+func IntrinsicGas(data []byte, contractCreation, homestead bool) (uint64, error) {
+
+	var gas uint64
+	if contractCreation && homestead {
+		gas = params.TxGasContractCreation
+	} else {
+		gas = params.TxGas
+	}
+
+	if len(data) > 0 {
+
+		var nz uint64
+		for _, byt := range data {
+			if byt != 0 {
+				nz++
+			}
+		}
+
+		nonZeroGas := params.TxDataNonZeroGasFrontier
+		if (math.MaxUint64-gas)/nonZeroGas < nz {
+			return 0, vm.ErrOutOfGas
+		}
+		gas += nz * nonZeroGas
+
+		z := uint64(len(data)) - nz
+		if (math.MaxUint64-gas)/params.TxDataZeroGas < z {
+			return 0, vm.ErrOutOfGas
+		}
+		gas += z * params.TxDataZeroGas
+	}
+	return gas, nil
+}
+
+func NewStateTransition(evm *vm.EVM, msg Message, gp *GasPool) *StateTransition {
+	return &StateTransition{
+		gp:       gp,
+		evm:      evm,
+		msg:      msg,
+		gasPrice: msg.GasPrice(),
+		value:    msg.Value(),
+		data:     msg.Data(),
+		state:    evm.StateDB,
+	}
+}
+
+func ApplyMessage(evm *vm.EVM, msg Message, gp *GasPool) (*ExecutionResult, error) {
+	return NewStateTransition(evm, msg, gp).TransitionDb()
+}
+
+func (st *StateTransition) from() vm.AccountRef {
+	f := st.msg.From()
+	if !st.state.Exist(f) {
+		st.state.CreateAccount(f)
+	}
+	return vm.AccountRef(f)
+}
+
+func (st *StateTransition) to() vm.AccountRef {
+	if st.msg == nil {
+		return vm.AccountRef{}
+	}
+	to := st.msg.To()
+	if to == nil {
+		return vm.AccountRef{}
+	}
+
+	reference := vm.AccountRef(*to)
+	if !st.state.Exist(*to) {
+		st.state.CreateAccount(*to)
+	}
+	return reference
+}
+
+func (st *StateTransition) useGas(amount uint64) error {
+	if st.gas < amount {
+		return vm.ErrOutOfGas
+	}
+	st.gas -= amount
+
+	return nil
+}
+
+func (st *StateTransition) buyGas() error {
+	var (
+		state  = st.state
+		sender = st.from()
+	)
+	mgval := new(big.Int).Mul(new(big.Int).SetUint64(st.msg.Gas()), st.gasPrice)
+	if state.GetBalance(sender.Address()).Cmp(mgval) < 0 {
+		return errInsufficientBalanceForGas
+	}
+	if err := st.gp.SubGas(st.msg.Gas()); err != nil {
+		return err
+	}
+	st.gas += st.msg.Gas()
+
+	st.initialGas = st.msg.Gas()
+	state.SubBalance(sender.Address(), mgval)
+	return nil
+}
+
+func (st *StateTransition) preCheck() error {
+	msg := st.msg
+	sender := st.from()
+
+	if msg.CheckNonce() {
+		nonce := st.state.GetNonce(sender.Address())
+		if nonce < msg.Nonce() {
+			return ErrNonceTooHigh
+		} else if nonce > msg.Nonce() {
+			return ErrNonceTooLow
+		}
+	}
+	return st.buyGas()
+}
+
+func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
+	if err := st.preCheck(); err != nil {
+		return nil, err
+	}
+	msg := st.msg
+	sender := st.from()
+
+	homestead := st.evm.ChainConfig().IsHomestead(st.evm.BlockNumber)
+	contractCreation := msg.To() == nil
+
+	gas, err := IntrinsicGas(st.data, contractCreation, homestead)
+	if err != nil {
+		return nil, err
+	}
+	if err = st.useGas(gas); err != nil {
+		return nil, err
+	}
+
+	var (
+		evm = st.evm
+
+		vmerr error
+		ret   []byte
+	)
+	if contractCreation {
+		ret, _, st.gas, vmerr = evm.Create(sender, st.data, st.gas, st.value)
+	} else {
+
+		st.state.SetNonce(sender.Address(), st.state.GetNonce(sender.Address())+1)
+		ret, st.gas, vmerr = evm.Call(sender, st.to().Address(), st.data, st.gas, st.value)
+	}
+
+	st.refundGas()
+	st.state.AddBalance(st.evm.Coinbase, new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice))
+
+	return &ExecutionResult{
+		UsedGas:    st.gasUsed(),
+		Err:        vmerr,
+		ReturnData: ret,
+	}, nil
+}
+
+func (st *StateTransition) refundGas() {
+
+	refund := st.gasUsed() / 2
+	if refund > st.state.GetRefund() {
+		refund = st.state.GetRefund()
+	}
+	st.gas += refund
+
+	sender := st.from()
+
+	remaining := new(big.Int).Mul(new(big.Int).SetUint64(st.gas), st.gasPrice)
+
+	st.state.AddBalance(sender.Address(), remaining)
+
+	st.gp.AddGas(st.gas)
+}
+
+func (st *StateTransition) gasUsed() uint64 {
+	return st.initialGas - st.gas
+}
+
+func ApplyMessageEx(evm *vm.EVM, msg Message, gp *GasPool) (*ExecutionResult, *big.Int, error) {
+	return NewStateTransition(evm, msg, gp).TransitionDbEx()
+}
+
+func (st *StateTransition) TransitionDbEx() (*ExecutionResult, *big.Int, error) {
+
+	if err := st.preCheck(); err != nil {
+		return nil, nil, err
+	}
+	msg := st.msg
+	sender := st.from()
+
+	homestead := st.evm.ChainConfig().IsHomestead(st.evm.BlockNumber)
+	contractCreation := msg.To() == nil
+
+	gas, err := IntrinsicGas(st.data, contractCreation, homestead)
+	if err != nil {
+		return nil, nil, err
+	}
+	if err = st.useGas(gas); err != nil {
+		return nil, nil, err
+	}
+
+	var (
+		evm = st.evm
+
+		vmerr error
+		ret   []byte
+	)
+
+	if contractCreation {
+		ret, _, st.gas, vmerr = evm.Create(sender, st.data, st.gas, st.value)
+	} else {
+
+		st.state.SetNonce(sender.Address(), st.state.GetNonce(sender.Address())+1)
+		ret, st.gas, vmerr = evm.Call(sender, st.to().Address(), st.data, st.gas, st.value)
+
+	}
+
+	st.refundGas()
+
+	usedMoney := new(big.Int).Mul(new(big.Int).SetUint64(st.gasUsed()), st.gasPrice)
+
+	return &ExecutionResult{
+		UsedGas:    st.gasUsed(),
+		Err:        vmerr,
+		ReturnData: ret,
+	}, usedMoney, nil
+}
