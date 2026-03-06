2909bc07dfad4247ce193483baf3f69a185c6bb6
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-03-06 18:13:20 +0200

    Create logger_test.go

---
+++ chain/core/vm/logger_test.go
@@ -0,0 +1,54 @@
+package vm
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/skaindev/skaina/chain/core/state"
+	"github.com/skaindev/skaina/params"
+	"github.com/skaindev/skaina/utilities/common"
+)
+
+type dummyContractRef struct {
+	calledForEach bool
+}
+
+func (dummyContractRef) ReturnGas(*big.Int)          {}
+func (dummyContractRef) Address() common.Address     { return common.Address{} }
+func (dummyContractRef) Value() *big.Int             { return new(big.Int) }
+func (dummyContractRef) SetCode(common.Hash, []byte) {}
+func (d *dummyContractRef) ForEachStorage(callback func(key, value common.Hash) bool) {
+	d.calledForEach = true
+}
+func (d *dummyContractRef) SubBalance(amount *big.Int) {}
+func (d *dummyContractRef) AddBalance(amount *big.Int) {}
+func (d *dummyContractRef) SetBalance(*big.Int)        {}
+func (d *dummyContractRef) SetNonce(uint64)            {}
+func (d *dummyContractRef) Balance() *big.Int          { return new(big.Int) }
+
+type dummyStatedb struct {
+	state.StateDB
+}
+
+func (*dummyStatedb) GetRefund() uint64 { return 1337 }
+
+func TestStoreCapture(t *testing.T) {
+	var (
+		env      = NewEVM(Context{}, &dummyStatedb{}, params.TestChainConfig, Config{})
+		logger   = NewStructLogger(nil)
+		mem      = NewMemory()
+		stack    = newstack()
+		contract = NewContract(&dummyContractRef{}, &dummyContractRef{}, new(big.Int), 0)
+	)
+	stack.push(big.NewInt(1))
+	stack.push(big.NewInt(0))
+	var index common.Hash
+	logger.CaptureState(env, 0, SSTORE, 0, 0, mem, stack, contract, 0, nil)
+	if len(logger.changedValues[contract.Address()]) == 0 {
+		t.Fatalf("expected exactly 1 changed value on address %x, got %d", contract.Address(), len(logger.changedValues[contract.Address()]))
+	}
+	exp := common.BigToHash(big.NewInt(1))
+	if logger.changedValues[contract.Address()][index] != exp {
+		t.Errorf("expected %x, got %x", exp, logger.changedValues[contract.Address()][index])
+	}
+}
