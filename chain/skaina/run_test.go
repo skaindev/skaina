26e9b8815faf7a380a2cbaba9e44a6d869b82316
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-03-13 17:35:59 +0200

    Create run_test.go

---
+++ chain/skaina/run_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/docker/docker/pkg/reexec"
+	"github.com/skaindev/skaina/internal/cmdtest"
+)
+
+func tmpdir(t *testing.T) string {
+	dir, err := ioutil.TempDir("", "skaina-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	return dir
+}
+
+type testneatchain struct {
+	*cmdtest.TestCmd
+
+	Datadir   string
+	Etherbase string
+}
+
+func init() {
+
+	reexec.Register("skaina-test", func() {
+		if err := app.Run(os.Args); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	})
+}
+
+func TestMain(m *testing.M) {
+
+	if reexec.Init() {
+		return
+	}
+	os.Exit(m.Run())
+}
+
+func runneatchain(t *testing.T, args ...string) *testneatchain {
+	tt := &testneatchain{}
+	tt.TestCmd = cmdtest.NewTestCmd(t, tt)
+	for i, arg := range args {
+		switch {
+		case arg == "-datadir" || arg == "--datadir":
+			if i < len(args)-1 {
+				tt.Datadir = args[i+1]
+			}
+		case arg == "-etherbase" || arg == "--etherbase":
+			if i < len(args)-1 {
+				tt.Etherbase = args[i+1]
+			}
+		}
+	}
+	if tt.Datadir == "" {
+		tt.Datadir = tmpdir(t)
+		tt.Cleanup = func() { os.RemoveAll(tt.Datadir) }
+		args = append([]string{"-datadir", tt.Datadir}, args...)
+
+		defer func() {
+			if t.Failed() {
+				tt.Cleanup()
+			}
+		}()
+	}
+
+	tt.Run("skaina-test", args...)
+
+	return tt
+}
