571f997a47a0f185b3e1f6875793d0c6291d2906
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-03-14 05:49:26 +0200

    Create errors.go

---
+++ chain/trie/errors.go
@@ -0,0 +1,16 @@
+package trie
+
+import (
+	"fmt"
+
+	"github.com/skaindev/skaina/utilities/common"
+)
+
+type MissingNodeError struct {
+	NodeHash common.Hash
+	Path     []byte
+}
+
+func (err *MissingNodeError) Error() string {
+	return fmt.Sprintf("missing trie node %x (path %x)", err.NodeHash, err.Path)
+}
