6d8bea9877df0456ba23fa43cbd02289442f467a
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-05-10 06:18:29 +0300

    Create miner.go

---
+++ utilities/miner/miner.go
@@ -0,0 +1,161 @@
+package miner
+
+import (
+	"fmt"
+	"sync/atomic"
+
+	"github.com/skaindev/skaina/chain/accounts"
+	"github.com/skaindev/skaina/chain/consensus"
+	"github.com/skaindev/skaina/chain/core"
+	"github.com/skaindev/skaina/chain/core/state"
+	"github.com/skaindev/skaina/chain/core/types"
+	"github.com/skaindev/skaina/chain/log"
+	"github.com/skaindev/skaina/neatdb"
+	"github.com/skaindev/skaina/neatptc/downloader"
+	"github.com/skaindev/skaina/params"
+	"github.com/skaindev/skaina/utilities/common"
+	"github.com/skaindev/skaina/utilities/event"
+)
+
+type Backend interface {
+	AccountManager() *accounts.Manager
+	BlockChain() *core.BlockChain
+	TxPool() *core.TxPool
+	ChainDb() neatdb.Database
+}
+
+type Pending interface {
+	Pending() (*types.Block, *state.StateDB)
+	PendingBlock() *types.Block
+}
+
+type Miner struct {
+	mux *event.TypeMux
+
+	worker *worker
+
+	coinbase common.Address
+	mining   int32
+	eth      Backend
+	engine   consensus.Engine
+	exitCh   chan struct{}
+
+	canStart    int32
+	shouldStart int32
+
+	logger log.Logger
+	cch    core.CrossChainHelper
+}
+
+func New(eth Backend, config *params.ChainConfig, mux *event.TypeMux, engine consensus.Engine, gasFloor, gasCeil uint64, cch core.CrossChainHelper) *Miner {
+	miner := &Miner{
+		eth:      eth,
+		mux:      mux,
+		engine:   engine,
+		exitCh:   make(chan struct{}),
+		worker:   newWorker(config, engine, eth, mux, gasFloor, gasCeil, cch),
+		canStart: 1,
+		logger:   config.ChainLogger,
+		cch:      cch,
+	}
+	miner.Register(NewCpuAgent(eth.BlockChain(), engine, config.ChainLogger))
+	go miner.update()
+
+	return miner
+}
+
+func (self *Miner) update() {
+	events := self.mux.Subscribe(downloader.StartEvent{}, downloader.DoneEvent{}, downloader.FailedEvent{})
+	defer events.Unsubscribe()
+
+	for {
+		select {
+		case ev := <-events.Chan():
+			if ev == nil {
+				return
+			}
+			switch ev.Data.(type) {
+			case downloader.StartEvent:
+				self.logger.Debug("(self *Miner) update(); downloader.StartEvent received")
+				atomic.StoreInt32(&self.canStart, 0)
+				if self.Mining() {
+					self.Stop()
+					atomic.StoreInt32(&self.shouldStart, 1)
+					self.logger.Info("Mining aborted due to sync")
+				}
+			case downloader.DoneEvent, downloader.FailedEvent:
+
+				self.logger.Debug("(self *Miner) update(); downloader.DoneEvent, downloader.FailedEvent received")
+				shouldStart := atomic.LoadInt32(&self.shouldStart) == 1
+
+				atomic.StoreInt32(&self.canStart, 1)
+				atomic.StoreInt32(&self.shouldStart, 0)
+				if shouldStart {
+					self.Start(self.coinbase)
+				}
+
+				return
+			}
+		case <-self.exitCh:
+			return
+		}
+	}
+}
+
+func (self *Miner) Start(coinbase common.Address) {
+	atomic.StoreInt32(&self.shouldStart, 1)
+	self.SetCoinbase(coinbase)
+
+	if atomic.LoadInt32(&self.canStart) == 0 {
+		self.logger.Info("Network syncing, will start miner afterwards")
+		return
+	}
+	self.worker.start()
+	self.worker.commitNewWork()
+}
+
+func (self *Miner) Stop() {
+	self.worker.stop()
+	atomic.StoreInt32(&self.shouldStart, 0)
+}
+
+func (self *Miner) Close() {
+	self.worker.close()
+	close(self.exitCh)
+}
+
+func (self *Miner) Register(agent Agent) {
+	if self.Mining() {
+		agent.Start()
+	}
+	self.worker.register(agent)
+}
+
+func (self *Miner) Unregister(agent Agent) {
+	self.worker.unregister(agent)
+}
+
+func (self *Miner) Mining() bool {
+	return self.worker.isRunning()
+}
+
+func (self *Miner) SetExtra(extra []byte) error {
+	if uint64(len(extra)) > params.MaximumExtraDataSize {
+		return fmt.Errorf("Extra exceeds max length. %d > %v", len(extra), params.MaximumExtraDataSize)
+	}
+	self.worker.setExtra(extra)
+	return nil
+}
+
+func (self *Miner) Pending() (*types.Block, *state.StateDB) {
+	return self.worker.pending()
+}
+
+func (self *Miner) PendingBlock() *types.Block {
+	return self.worker.pendingBlock()
+}
+
+func (self *Miner) SetCoinbase(addr common.Address) {
+	self.coinbase = addr
+	self.worker.setCoinbase(addr)
+}
